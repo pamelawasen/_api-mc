a5209342c97cc7b056124517920b5ad7cfdd8979
OtavioCapel <otavio_capel@hotmail.com>
2019-01-01 21:40:51 -0200

    crud client

--- src/app/routes/client.js
+++ src/app/routes/client.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const clientModel = require('../models/client');
 
-
 const router = express.Router();
 
-router.post('/create/client', async (req, res) => {
+
+/*
+* Rota para criaçao de um novo client
+*/
+router.put('/create/client', async (req, res) => {
     //desestruturação no req.body
     const { email, cpf } = req.body;
     
@@ -17,6 +20,54 @@ router.post('/create/client', async (req, res) => {
         const user = await clientModel.create(req.body);
         //retorna status 200 e o client criado
         return res.status(200).send(user);
+
+    }
+    catch(err){
+        //aqui vamos validar os erros
+
+    }
+})
+
+/*
+* Rota para alterar um client
+*/
+router.post('/update/client', async (req, res) => {
+    //desestruturação no req.body
+    var { _id } = req.body;
+    
+    try {
+        //verifica se existe um usuario com o id passado
+        if( !await clientModel.findById(_id) )
+            return res.status(400).send('Usuario não encontrado');//caso nao existe manda msg de erro      
+
+        //localiza e faz update do cliente
+        await clientModel.findOneAndUpdate(_id, { 
+            $set: req.body 
+        })
+        return res.status(200).send('ok')
+
+    }
+    catch(err){
+        //aqui vamos validar os erros
+
+    }
+})
+
+/*
+* Rota para deletar um  client
+*/
+router.delete('/delete/client/:_id', async (req, res) => {
+    //desestruturação no req.params
+    var { _id } = req.params;
+    
+    try {
+        //verifica se existe um usuario com o id passado
+        if( !await clientModel.findById(_id) )
+            return res.status(400).send('Usuario não encontrado');//caso nao existe manda msg de erro      
+
+        //localiza e faz delete do cliente
+        await clientModel.findByIdAndDelete(_id)
+        return res.status(200).send('deletado')
 
     }
     catch(err){
